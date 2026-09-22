88e15be955e1f8ab53388c04ff6691ae62bff37f
gammaal <gammaalfatah@smkwikrama.sch.id>
2026-09-22 14:14:41 +0700

    fix: sqlite path untuk vercel /tmp

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
 
+// Buat SQLite database di /tmp (writable di Vercel)
+$dbPath = '/tmp/database.sqlite';
+if (!file_exists($dbPath)) {
+    touch($dbPath);
+}
+
+// Set environment variable agar Laravel pakai /tmp/database.sqlite
+$_ENV['DB_DATABASE'] = $dbPath;
+putenv('DB_DATABASE=' . $dbPath);
+
 require __DIR__ . '/../public/index.php';
